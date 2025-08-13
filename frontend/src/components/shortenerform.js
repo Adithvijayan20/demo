64a1edcc652579c1523ed5f71be96581197664dc
Adith vijayan <152381037+Adithvijayan20@users.noreply.github.com>
2025-08-14 01:01:53 +0530

    commit

--- frontend/src/components/shortenerform.js
+++ frontend/src/components/shortenerform.js
@@ -9,7 +9,7 @@ function ShortenerForm({ onShorten }) {
     setLoading(true);
     try {
       // const res = await fetch("http://localhost:8000/shorten", {
-      const res = await fetch("https://demo-1-wngg.onrender.com/shorten", {
+      const res = await fetch("https:/shorten", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url }),
